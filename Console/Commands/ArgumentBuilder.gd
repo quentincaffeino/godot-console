726d8953e9475294dae98f3467a920feb581c5bc
Sergei ZH <sergey95aeses@gmail.com>
2017-12-27 10:44:47 +0100

    Default fallback to type Any

--- Console/Commands/ArgumentBuilder.gd
+++ Console/Commands/ArgumentBuilder.gd
@@ -12,8 +12,8 @@ static func build(_name, _type = 0):  # Argument|int
 	var type
 	if typeof(_type) == TYPE_OBJECT and _type is BaseType:
 		type = _type
-	elif typeof(_type) == TYPE_INT:
-		type = TypesBuilder.build(_type)
+	else:
+		type = TypesBuilder.build(_type if typeof(_type) == TYPE_INT else 0)
 
 	if typeof(type) == TYPE_INT:
 		return FAILED
@@ -28,7 +28,7 @@ static func buildAll(args):  # Array<Argument>|int
 	var tArg
 	for arg in args:
 		match typeof(arg):
-			TYPE_ARRAY:            tArg = build(arg[0], arg[1])
+			TYPE_ARRAY:            tArg = build(arg[0], arg[1] if arg.size() > 1 else 0)
 			TYPE_STRING:           tArg = build(arg)
 			TYPE_OBJECT, TYPE_INT: tArg = build(null, arg)
 
--- Console/Commands/Types/TypesBuilder.gd
+++ Console/Commands/Types/TypesBuilder.gd
@@ -2,20 +2,15 @@
 extends Object
 
 
-const TypesList = {
-	'0': preload('Any.gd'),
-	'1': preload('Bool.gd'),
-	'2': preload('Int.gd'),
-	'3': preload('Float.gd'),
-	'4': preload('String.gd'),
-}
+const TypesList = [
+	preload('Any.gd'),
+	preload('Bool.gd'),
+	preload('Int.gd'),
+	preload('Float.gd'),
+	preload('String.gd'),
+]
 
 
-# @param  int  _type
-static func build(_type):  # BaseType|int
-	var str_type = str(_type)
-
-	if (TypesList.has(str_type)):
-		return TypesList[str_type].new()
-
-	return FAILED
+# @param  int  type
+static func build(type):  # BaseType
+	return TypesList[type if type < TypesList.size() else 0].new()
