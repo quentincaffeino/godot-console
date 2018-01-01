015ec318100ae5f44b42af244d243444959aef2c
Sergei ZH <sergey95aeses@gmail.com>
2018-01-01 02:19:22 +0100

    Maybe it would be better like this

--- Console/Commands/Types/Bool.gd
+++ Console/Commands/Types/Bool.gd
@@ -11,6 +11,6 @@ func get():  # bool
 	if _rematch and _rematch is RegExMatch:
 		var tmp = _rematch.get_string()
 
-		return false if tmp == '0' or tmp == 'false' else true
+		return true if tmp == '1' or tmp == 'true' else false
 
 	return false
