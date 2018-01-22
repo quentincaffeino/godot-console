bb95fb2b043f9567f91c7389f64c7fe3cf7ec1ce
Sergei ZH <sergey95aeses@gmail.com>
2018-01-22 16:11:32 +0100

    Small optimizations on callback class

--- Console/Callback.gd
+++ Console/Callback.gd
@@ -3,6 +3,7 @@ extends Object
 
 
 enum TYPE {
+	UNKNOWN,
 	VARIABLE,
 	METHOD
 }
@@ -15,19 +16,25 @@ var _target
 var _name
 
 # @var  int
-var _type = -1
+var _type
 
 
 # @param  Object  target
 # @param  string  name
-func _init(target, name):
+# @param  int     type
+func _init(target, name, type = UNKNOWN):
 	_target = target
 	_name = name
-	_type = getType(_target, _name)
+
+	if type == UNKNOWN:
+		type = getType(_target, _name)
+
+	_type = type
 
 
 # @param  Array<Variant>  argv
 func call(argv = []):  # Variant
+	# If passed argv as any value except Array, wrap it into array
 	if typeof(argv) <= TYPE_ARRAY and typeof(argv) >= TYPE_COLOR_ARRAY:
 		argv = [argv]
 
@@ -36,11 +43,16 @@ func call(argv = []):  # Variant
 		return _target.get(_name)
 	elif _type == METHOD:
 		return _target.callv(_name, argv)
+	
+	Console.Log.error('Unable to call [b]unknown type[/b].')
 
 
+# Use this method to create a callback
+#
 # @param  Object  target
 # @param  string  name
-static func create(target, name):  # Callback
+# @param  int     type
+static func create(target, name, type = UNKNOWN):  # Callback
 	if typeof(target) != TYPE_OBJECT:
 		Console.Log.error('First argument must be target object.')
 		return FAILED
@@ -49,11 +61,14 @@ static func create(target, name):  # Callback
 		Console.Log.error('Second argument must be variable or method name.')
 		return FAILED
 
-	if getType(target, name) < 0:
-		Console.Log.error('Target object doesn\'t have supplied method or variable')
-		return FAILED
+	if type == UNKNOWN:
+		type = getType(target, name)
 
-	return Console.Callback.new(target, name)
+		if type == UNKNOWN:
+			Console.Log.error('Target object doesn\'t have supplied method or variable')
+			return FAILED
+
+	return Console.Callback.new(target, name, type)
 
 
 # @param  Object  target
@@ -70,4 +85,4 @@ static func getType(target, name):  # int
 		if p.name == name:
 			return VARIABLE
 
-	return -1
+	return UNKNOWN
