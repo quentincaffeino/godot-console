5be444eda2c2de3e2e1aa2a8f3754604c8658505
Sergei ZH <sergey95aeses@gmail.com>
2018-01-22 16:32:20 +0100

    Small fix on unrequired description and code refactor

--- Console/Commands/CommandBuilder.gd
+++ Console/Commands/CommandBuilder.gd
@@ -1,5 +1,6 @@
 
 extends Object
+
 const Command = preload('Command.gd')
 const ArgumentB = preload('ArgumentBuilder.gd')
 
@@ -9,20 +10,31 @@ const ArgumentB = preload('ArgumentBuilder.gd')
 static func build(alias, params):  # Command
 	# Warn
 	if params.has('type'):
-		Console.Log.warn('Using deprecated argument [b]type[/b] in [b]' + alias + '[/b].')
+		Console.Log.warn(\
+			'Using deprecated argument [b]type[/b] in [b]' + alias + '[/b].')
 	if params.has('name'):
-		Console.Log.warn('Using deprecated argument [b]name[/b] in [b]' + alias + '[/b].')
+		Console.Log.warn(\
+			'Using deprecated argument [b]name[/b] in [b]' + alias + '[/b].')
 
 	# Check target
 	if !params.has('target') and !params.target:
-		Console.Log.error('Failed to register [b]' + alias + '[/b]. Missing [b]target[/b] parametr.')
+		Console.Log.error(\
+			'Failed to register [b]' + alias + '[/b]. Missing [b]target[/b] parametr.')
 		return
 
 	# Create target if old style used
-	if typeof(params.target) != TYPE_OBJECT or !(params.target is Console.Callback):
-		var target = params.target[0] if typeof(params.target) == TYPE_ARRAY else params.target
+	if typeof(params.target) != TYPE_OBJECT or \
+			!(params.target is Console.Callback):
+
+		var target = params.target
+		if typeof(params.target) == TYPE_ARRAY:
+			target = params.target[0]
+
 		var name = alias
-		if typeof(params.target) == TYPE_ARRAY and params.target.size() > 1 and typeof(params.target[1]) == TYPE_STRING:
+
+		if typeof(params.target) == TYPE_ARRAY and \
+				params.target.size() > 1 and \
+				typeof(params.target[1]) == TYPE_STRING:
 			name = params.target[1]
 		elif params.has('name'):
 			name = params.name
@@ -30,11 +42,14 @@ static func build(alias, params):  # Command
 		params.target = Console.Callback.create(target, name)
 
 	if !params.target:
-		Console.Log.error('Failed to register [b]' + alias + '[/b]. Failed to create callback to target')
+		Console.Log.error(\
+			'Failed to register [b]' + alias + \
+			'[/b]. Failed to create callback to target')
 		return
 
 	# Set arguments
 	if params.target._type == Console.Callback.VARIABLE and params.has('args'):
+		# Ignore all arguments except first cause variable takes only one arg
 		params.args = [params.args[0]]
 
 	if params.has('arg'):
@@ -46,8 +61,13 @@ static func build(alias, params):  # Command
 		params.args = []
 
 	if typeof(params.args) == TYPE_INT:
-		Console.Log.error('Failed to register [b]' + alias + '[/b]. Wrong [b]arguments[/b] parametr.')
+		Console.Log.error(\
+			'Failed to register [b]' + alias + \
+			'[/b]. Wrong [b]arguments[/b] parametr.')
 		return
+
+	if !params.has('description'):
+		params.description = null
 
 
 	return Command.new(alias, params.target, params.args, params.description)
