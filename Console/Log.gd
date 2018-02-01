4d4d87cd456583afc7b979d4274edca3d957578d
Sergei ZH <sergey95aeses@gmail.com>
2018-02-02 00:02:10 +0100

    Added debug method in Log

--- Console/Log.gd
+++ Console/Log.gd
@@ -55,11 +55,23 @@ func warn(message, debugInfo = ''):  # void
 
 # @param  string  message
 # @param  string  debugInfo
-func error(message, debugInfo = ''):
-	if logLevel <= ERROR:  # void
+func error(message, debugInfo = ''):  # void
+	if logLevel <= ERROR:
 		var write = '[color=red][ERROR][/color] '
 
 		if Console.debugMode and debugInfo:
+			write += str(debugInfo) + ': '
+
+		Console.writeLine(write + str(message))
+
+
+# @param  string  message
+# @param  string  debugInfo
+func debug(message, debugInfo = ''):  # void
+	if Console.debugMode:
+		var write = '[color=green][DEBUG][/color] '
+
+		if debugInfo:
 			write += str(debugInfo) + ': '
 
 		Console.writeLine(write + str(message))
--- Console/README.md
+++ Console/README.md
@@ -30,11 +30,12 @@
 
 | Methods | Description |
 |--|--|
-| *void* setLogLevel(*int* inLogLevel = INFO) | Messages lower than provided level won't show in console output |
+| *void* setLogLevel(*int* inLogLevel = INFO) | Messages lower than provided level won't be printed in console output |
 | *void* log(*string* message, *int* type = INFO) |  |
 | *void* info(*string* message, *string* debugInfo = '') |  |
 | *void* warn(*string* message, *string* debugInfo = '') |  |
 | *void* error(*string* message, *string* debugInfo = '') |  |
+| *void* debug(*string* message, *string* debugInfo = '') | Printed only if debug mode is enabled |
 
 #### Log level enum
 - INFO
