85ce619d6f0c1c4251e584b628a3efd8eca9f6ca
quentincaffeino <sergey95zu@gmail.com>
2021-01-11 00:26:56 +0100

    Docblock fix for Error class

--- godot/addons/quentincaffeino-console/src/Misc/Error.gd
+++ godot/addons/quentincaffeino-console/src/Misc/Error.gd
@@ -2,12 +2,12 @@
 # @var  String
 var _message
 
-# @var  int
+# @var  Variant|null
 var _code
 
 
-# @param  String  message
-# @param  int     code
+# @param  String        message
+# @param  Variant|null  code
 func _init(message, code = null):
   self._message = message
   self._code = code
@@ -18,7 +18,7 @@ func get_message():
   return self._message
 
 
-# @returns  int
+# @returns  Variant|null
 func get_code():
   return self._code
 
