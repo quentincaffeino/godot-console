1248bb5e5d86c51be31e6efc972c87217fe3134d
quentincaffeino <sergey95zu@gmail.com>
2021-07-22 14:29:45 +0200

    Added custom command to demo project to showoff the functionality

---
+++ demo/scene.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+onready var label = $VBoxContainer/Label
+
+
+func _ready():
+	Console.add_command("set_label_text", self, "_set_label_text")\
+		.add_argument("text", TYPE_STRING)\
+		.register()
+
+
+func _set_label_text(new_text):
+	label.text = new_text
