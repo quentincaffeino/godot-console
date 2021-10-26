850e6f2b78d4a822200b06d39bd7e6a422ae5f2a
Ryan <rlinehan2@gmail.com>
2021-10-25 18:45:34 -0700

    Removed ConsoleArgumentType. Changed method param to built in type

--- addons/quentincaffeino/console-csharp/src/CSharp/CSharpConsole.cs
+++ addons/quentincaffeino/console-csharp/src/CSharp/CSharpConsole.cs
@@ -24,7 +24,7 @@ public class CSharpCommand: Godot.Object {
 		return this;
 	}
 
-	public CSharpCommand AddArgument(string argumentName, ConsoleArgumentType argumentType) {
+	public CSharpCommand AddArgument(string argumentName, Variant.Type argumentType) {
 		_commandObject.Call("add_argument", argumentName, argumentType);
 		return this;
 	}
@@ -33,35 +33,4 @@ public class CSharpCommand: Godot.Object {
 		_commandObject.Call("register");
 		return this;
 	}
-}
-
-public enum ConsoleArgumentType {
-	TYPE_NIL = 0,
-	TYPE_BOOL = 1,
-	TYPE_INT = 2,
-	TYPE_REAL = 3,
-	TYPE_STRING = 4,
-	TYPE_VECTOR2 = 5,
-	TYPE_RECT2 = 6,
-	TYPE_VECTOR3 = 7,
-	TYPE_TRANFORM2D = 8,
-	TYPE_PLANE = 9,
-	TYPE_QUAT = 10,
-	TYPE_AABB = 11,
-	TYPE_BASIS = 12,
-	TYPE_TRANSFORM = 13,
-	TYPE_COLOR = 14,
-	TYPE_NODE_PATH = 15,
-	TYPE_RID = 16,
-	TYPE_OBJECT = 17,
-	TYPE_DICTIONARY = 18,
-	TYPE_ARRAY = 19,
-	TYPE_RAW_ARRAY = 20,
-	TYPE_INT_ARRAY = 21,
-	TYPE_REAL_ARRAY = 22,
-	TYPE_STRING_ARRAY = 23,
-	TYPE_VECTOR2_ARRAY = 24,
-	TYPE_VECTOR3_ARRAY = 25,
-	TYPE_COLOR_ARRAY = 26,
-	TYPE_MAX = 27
 }
