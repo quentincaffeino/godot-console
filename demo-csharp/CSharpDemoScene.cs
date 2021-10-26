4925e650c2a528e4d7f238e15575f6a5126fb5f5
Ryan <rlinehan2@gmail.com>
2021-10-25 19:09:04 -0700

    Updated demo csharp project to use new class names

--- demo-csharp/CSharpDemoScene.cs
+++ demo-csharp/CSharpDemoScene.cs
@@ -2,14 +2,14 @@ using Godot;
 
 public class CSharpDemoScene : CanvasLayer
 {    
-	CSharpConsole _wrapper;
+	Console _wrapper;
 	Label _label;
 	public override void _Ready()
 	{
-		_wrapper = GetTree().Root.GetNode<CSharpConsole>("CSharpConsole");        
+		_wrapper = GetTree().Root.GetNode<Console>("CSharpConsole");        
 		_wrapper.AddCommand("change_csharp_label", this, nameof(ChangeLabelText))
 				.SetDescription("Changes label to %newText%")
-				.AddArgument("newText", ConsoleArgumentType.TYPE_STRING)
+				.AddArgument("newText", Variant.Type.String)
 				.Register();
 		_label = GetNode<Label>("Label");
 	}
