8041c5f6c1c060fc393cb9a5b2ae78009f2a9fab
Ryan <rlinehan2@gmail.com>
2021-10-31 21:37:09 -0700

    Fixed spaces and tabs mixing. Renamed ConsoleCommand to CommandBuilder

--- addons/quentincaffeino/console-csharp/src/Console.cs
+++ addons/quentincaffeino/console-csharp/src/Console.cs
@@ -7,8 +7,8 @@ public class Console : Node
 		_console = GetTree().Root.GetNode<CanvasLayer>("Console");
 	}	
 
-	public ConsoleCommand AddCommand(string name, Godot.Object target, string targetMethodName) {
-		Godot.Object consoleCommand = _console.Call("add_command", name, target, targetMethodName) as Godot.Object;
-		return new ConsoleCommand(consoleCommand);
+	public CommandBuilder AddCommand(string name, Godot.Object target, string targetMethodName) {
+		Godot.Object consoleCommand = _console.Call("add_command", name, target, targetMethodName) as Godot.Object;		
+		return new CommandBuilder(consoleCommand);
 	}
 }
