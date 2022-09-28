48563088f11e840f43787ba760ec1e90844df9ff
quentincaffeino <sergey95zu@gmail.com>
2022-09-28 13:13:50 +0200

    fix plugin.gd; ref move action registering to common actions from two plugins

--- addons/quentincaffeino/console/DefaultActions.gd
+++ addons/quentincaffeino/console/DefaultActions.gd
@@ -6,8 +6,8 @@ const console_toggle_props = {
 	"name": CONSOLE_TOGGLE,
 	"events": [
 		{
-			"command": true,
-			"scancode": KEY_QUOTELEFT,
+			"ctrl_pressed": true,
+			"keycode": KEY_QUOTELEFT,
 		}
 	]
 }
@@ -19,7 +19,7 @@ const console_autocomplete_props = {
 	"name": CONSOLE_AUTOCOMPLETE,
 	"events": [
 		{
-			"scancode": KEY_TAB,
+			"keycode": KEY_TAB,
 		}
 	]
 }
@@ -31,7 +31,7 @@ const console_history_up_props = {
 	"name": CONSOLE_HISTORY_UP,
 	"events": [
 		{
-			"scancode": KEY_UP,
+			"keycode": KEY_UP,
 		}
 	]
 }
@@ -43,7 +43,40 @@ const console_history_down_props = {
 	"name": CONSOLE_HISTORY_DOWN,
 	"events": [
 		{
-			"scancode": KEY_DOWN,
+			"keycode": KEY_DOWN,
 		}
 	]
 }
+
+# @var  Array
+const ACTIONS = [
+	console_toggle_props,
+	console_autocomplete_props,
+	console_history_up_props,
+	console_history_down_props
+]
+
+static func register():
+	# Register input events
+	for action_props in ACTIONS:
+		var setting_name = "input/" + action_props["name"]
+
+		if not ProjectSettings.has_setting(setting_name):
+			var events = []
+
+			var action_props_events = action_props["events"]
+
+			for event_data in action_props_events:
+				var event = InputEventKey.new()
+				for prop_name in event_data:
+					event.set(prop_name, event_data[prop_name])
+
+				events.append(event)
+
+			ProjectSettings.set_setting(setting_name, {
+				"deadzone": float(action_props["deadzone"] if "deadzone" in action_props else 0.5),
+				"events": events
+			})
+
+	var result = ProjectSettings.save()
+	assert(result == OK, "Failed to save project settings")
